f4be86586211a3a5c0eece5a230406a153e2d87f
Matt Low <matthewlowstx@gmail.com>
2025-02-21 06:22:16 -0700

    toggle opacity keybind

--- wezterm.lua
+++ wezterm.lua
@@ -14,6 +14,7 @@ config.max_fps = 60
 
 -- Window stuff
 config.window_decorations = "NONE | RESIZE"
+config.window_background_opacity = 0.9
 
 -- Colors
 config.color_scheme = "Bamboo"
@@ -21,7 +22,20 @@ config.color_scheme = "Bamboo"
 -- Keybinds
 config.keys = {
 	-- Toggle Opacity
-	
+	{
+		key = "9",
+		mods = "CTRL|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
 
 	-- Close pane
 	{
